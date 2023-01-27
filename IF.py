528f5be014f9167ba5f5aeb61d1002f567ecccbb
subhash kumar yadav <110653851+Subhashkunaryadav@users.noreply.github.com>
2023-01-27 09:58:56 +0530

    Add files via upload

---
+++ IF.py
@@ -0,0 +1,135 @@
+alien_color = 'green'
+if alien_color == 'green':
+ print("You just earned 5 points!")
+
+alien_color = 'green'
+if alien_color == 'green':
+ print("You just earned 5 points!")
+else:
+ print("You just earned 10 points!")
+
+alien_color = 'red'
+if alien_color == 'green':
+ print("You just earned 5 points!")
+elif alien_color == 'yellow':
+ print("You just earned 10 points!")
+else:
+ print("You just earned 15 points!")
+
+age = 17
+if age < 2:
+ print("You're a baby!")
+elif age < 4:
+ print("You're a toddler!")
+elif age < 13:
+ print("You're a kid!")
+elif age < 20:
+ print("You're a teenager!")
+elif age < 65:
+ print("You're an adult!")
+else:
+ print("You're an elder!")
+
+favorite_fruits = ['blueberries', 'salmonberries', 'peaches']
+if 'bananas' in favorite_fruits:
+ print("You really like bananas!")
+if 'apples' in favorite_fruits:
+ print("You really like apples!")
+if 'blueberries' in favorite_fruits:
+ print("You really like blueberries!")
+if 'kiwis' in favorite_fruits:
+ print("You really like kiwis!")
+if 'peaches' in favorite_fruits:
+ print("You really like peaches!")
+
+numbers = list(range(1,10))
+for number in numbers:
+ if number == 1:
+  print("1st")
+ elif number == 2:
+  print("2nd")
+ elif number == 3:
+  print("3rd")
+else:
+ print(f"{number}th")
+
+x = 19
+if x > 20:
+    print("This is the number twenty")
+elif x < 40:
+    print("This is the elif")
+else:
+    print("This is not the same")   
+
+y = 20
+if y > 19:
+    print("This is positive")
+elif y < 21:
+    print("This is the negative")
+else:
+    print("This is error")  
+
+
+a = 20
+if a > 19:
+    print("This is the positive")
+
+a = 5
+b = 10
+if a > 4 and b < 20:
+    print("These are the numbers")   
+
+a = 10
+b = 20
+if a > b:
+    print("This is the answer")      
+
+
+
+Numbers = 100
+if Numbers > 150:
+    print("This is greater")
+elif Numbers < 200:
+    print("This is ok")    
+else:
+    print("This is not")     
+
+
+cars = ['bmw', 'tata', 'kia']
+for car in cars:
+    if car == 'bmw':
+      print(car.upper())
+    else:
+        print(car.lower())  
+
+Names = ['subhash', 'kumar', 'yadav', 'bca']
+for Name in Names:
+    if Name == 'subhash':
+        print(Name.title())
+    else:
+        print(Name.upper())
+
+Letters = ['a', 'd', 'y', 'j']
+for Letter in Letters:
+    if Letter == 'j':
+        print(Letter.lower())
+    else:
+        print(Letter.upper())
+
+Numbers = ['10', '20', '30', '40']
+for Number in Numbers:
+    if Number == '30':
+        print("This is the number thirty")
+    else:
+        print("All other numbers") 
+        
+age = 20
+if age > 21:
+    print('Your are elder' )
+else:
+    print('You are not elder') 
+    print('Please become elder')   
+
+
+           
+
